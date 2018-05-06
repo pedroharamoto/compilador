1207a54a3d7abba560603850d8f5abdce041846b
Pedro Haramoto <pedroharamoto@gmail.com>
2018-05-06 01:10:30 -0300

    add analisador sintatico

---
+++ analisador_sintatico.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#data de inicio: 05/05/2018
+#
+# Pedro Vitor Jhum Haramoto RA: 161024157
+# João Pedro Marin Comini   RA: 161020551
+#
+
+#importando a classe do analisador lexico
+import analisador_lexico
+
+#gerando a tabela de tokens com a função get_tokens da classe do analisador lexico
+tokens = analisador_lexico.afd_lexico().get_tokens()
+
+#
+#   INICIO DA CLASSE A_SINTATICO()
+#
+class a_sintatico():
+    #
+    #
+    #
+    def __init__(self, tokens):
+        #construtor
+        self.show_tokens(tokens)
+    #
+    #
+    #
+    def show_tokens(self, tokens):
+        #mostra os tokens
+        for (i, token) in enumerate(tokens):
+            print(token[2],":")
+            for (j, elemento) in enumerate(token):
+               print("\t",elemento)
+
+re = a_sintatico(tokens)
