fb5ea528c1d48c6cc78d5e5e72b9e2a2fdc84204
Pedro Haramoto <pedroharamoto@gmail.com>
2018-07-02 22:36:25 -0300

    alterações nas funções term e factor

--- analisador_sintatico.py
+++ analisador_sintatico.py
@@ -27,8 +27,8 @@ class a_sintatico():
         self.token = self.get_token() #ja pega o primeiro token
         #MEPA#
         #**********#
-        self.cont_vars = 0
-        self.cod_mepa = []
+        self.cont_vars = 0 #variavel para contar a quantidade de variaveis que serao alocadas
+        self.cod_mepa = [] #variavel para guardar as instruções MEPA
         #**********#
     #
     #
@@ -98,6 +98,13 @@ class a_sintatico():
                     #
                     # fim do 'program xxx;'. Portanto, chama-se bloco()
                     #
+
+                    #MEPA#
+                    #**********#
+                    #como achei um program xxxx;, devo add a instrução mepa INPP
+                    self.cod_mepa.append("INPP")
+                    #
+                    #**********#
                     self.bloco()
                     #
                     if(self.token[0] == '&'): #não achou nada
@@ -264,6 +271,7 @@ class a_sintatico():
         #print('oi',self.token)
         #
         if(self.token[1] == 'ID'):
+            print(self.token)
             #statment para o tipo atribuição (a:= b+c;)
             self.buffer.append(self.token)
             self.token = self.get_token()
@@ -275,6 +283,7 @@ class a_sintatico():
             if(self.token[0] == ':='):
                 #se encontrar uma operação de atr, chamara a função para verificar se o proximo 'comando'
                 #é uma expressão
+                print(self.token)
                 self.buffer.append(self.token)
                 self.token = self.get_token()
                 self.exp()
@@ -560,7 +569,7 @@ class a_sintatico():
             self.buffer.append(self.token)
             self.token = self.get_token()
 
-            if((self.token[0] in ['+','-','or','*','/','div','mod','and'])):
+            if((self.token[1] not in ['ID','NUM_FLOAT','NUM_INT'])):
                 #erro do tipo a := a-++b;
                 self.err2(self.token)
 
@@ -577,10 +586,11 @@ class a_sintatico():
         self.factor()
 
         if(self.token[0] in ['*','/','div','mod','and']):
+            print(self.token)
             self.buffer.append(self.token)
             self.token = self.get_token()
 
-            if((self.token[0] in ['+','-','or','*','/','div','mod','and'])):
+            if((self.token[1] not in ['ID','NUM_FLOAT','NUM_INT'])):
                 #erro do tipo a := **mod a;
                 self.err2(self.token)
 
@@ -603,6 +613,7 @@ class a_sintatico():
                 self.token = self.get_token()
                 return
         elif(self.token[1] in ['ID','NUM_FLOAT','NUM_INT','STRING1','STRING2']):
+            print(self.token)
             self.buffer.append(self.token)
             self.token = self.get_token()
             return
@@ -692,4 +703,4 @@ sintatico = a_sintatico(tokens)
 sintatico.inicio()
 #sintatico.show_saida()
 #sintatico.show_tokens()
-sintatico.show_mepa()
+#sintatico.show_mepa()
